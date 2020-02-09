d79448e564fbc269956b9a3c3a5cd38268727fc6
Irena Jurasek <irenaskydive@gmail.com>
2020-02-09 12:54:56 +0100

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,5 +1,5 @@
 collect_ratings = () =>  {
-    ratings = {'count': 0, 'sum': 0, 'average': 0 };
+    const ratings = {'count': 0, 'sum': 0, 'average': 0 };
     let rating = 0;
     const elements = document.querySelectorAll('.rating');
     elements.forEach((element) => {
